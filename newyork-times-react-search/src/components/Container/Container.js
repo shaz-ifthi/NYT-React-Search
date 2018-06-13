938ab09e4854c333d811d7b336aab8ebf3bb260e
Shaz <sshaideen@yahoo.com>
2018-06-12 18:40:26 -0700

    cleaned up files

--- newyork-times-react-search/src/components/Container/Container.js
+++
@@ -1,9 +0,0 @@
-// Import React
-import React from "react";
-
-// Define component for container (layout/grid component)
-const Container = props =>
-  <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;
-
-// export the Container component.
-export default Container;
